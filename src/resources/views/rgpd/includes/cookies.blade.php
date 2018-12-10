a032b96da8c82255d52517372b5dd04545508474
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-12-10 11:31:40 +0100

    add subfolders

--- src/resources/views/rgpd/includes/cookies.blade.php
+++ src/resources/views/rgpd/includes/cookies.blade.php
@@ -1,69 +1,69 @@
 <div class="rgpd">
-    <h2>{!! __('rgpd-cookies.cookies_header') !!}</h2>
+    <h2>{!! __('rgpd/market/cookies.cookies_header') !!}</h2>
 
     <div>
-        <h3>{!! __('rgpd-cookies.cookies_1') !!}</h3>
-        <p>{!! __('rgpd-cookies.cookies_1_1') !!}</p>
-        <p>{!! __('rgpd-cookies.cookies_1_2') !!}</p>
+        <h3>{!! __('rgpd/market/cookies.cookies_1') !!}</h3>
+        <p>{!! __('rgpd/market/cookies.cookies_1_1') !!}</p>
+        <p>{!! __('rgpd/market/cookies.cookies_1_2') !!}</p>
 
-        <h3>{!! __('rgpd-cookies.cookies_2') !!}</h3>
-        <p>{!! __('rgpd-cookies.cookies_2_1', ['trademark' => config('pulsar-rgpd.trademark')]) !!}</p>
+        <h3>{!! __('rgpd/market/cookies.cookies_2') !!}</h3>
+        <p>{!! __('rgpd/market/cookies.cookies_2_1', ['trademark' => config('pulsar-rgpd.trademark')]) !!}</p>
 
-        <h3>{!! __('rgpd-cookies.cookies_3') !!}</h3>
-        <p>{!! __('rgpd-cookies.cookies_3_1') !!}</p>
-        <p>{!! __('rgpd-cookies.cookies_3_2') !!}</p>
+        <h3>{!! __('rgpd/market/cookies.cookies_3') !!}</h3>
+        <p>{!! __('rgpd/market/cookies.cookies_3_1') !!}</p>
+        <p>{!! __('rgpd/market/cookies.cookies_3_2') !!}</p>
 
-        <h5>{!! __('rgpd-cookies.cookies_3_3') !!}</h5>
-        <p>{!! __('rgpd-cookies.cookies_3_3_1') !!}</p>
+        <h5>{!! __('rgpd/market/cookies.cookies_3_3') !!}</h5>
+        <p>{!! __('rgpd/market/cookies.cookies_3_3_1') !!}</p>
         <ul>
-            <li><p>{!! __('rgpd-cookies.cookies_3_3_2') !!}</p></li>
-            <li><p>{!! __('rgpd-cookies.cookies_3_3_3') !!}</p></li>
+            <li><p>{!! __('rgpd/market/cookies.cookies_3_3_2') !!}</p></li>
+            <li><p>{!! __('rgpd/market/cookies.cookies_3_3_3') !!}</p></li>
         </ul>
-        <p>{!! __('rgpd-cookies.cookies_3_3_4') !!}</p>
-        <p>{!! __('rgpd-cookies.cookies_3_3_5') !!}</p>
-        <p>{!! __('rgpd-cookies.cookies_3_3_6') !!}</p>
+        <p>{!! __('rgpd/market/cookies.cookies_3_3_4') !!}</p>
+        <p>{!! __('rgpd/market/cookies.cookies_3_3_5') !!}</p>
+        <p>{!! __('rgpd/market/cookies.cookies_3_3_6') !!}</p>
 
-        <h5>{!! __('rgpd-cookies.cookies_3_4') !!}</h5>
-        <p>{!! __('rgpd-cookies.cookies_3_4_1') !!}</p>
+        <h5>{!! __('rgpd/market/cookies.cookies_3_4') !!}</h5>
+        <p>{!! __('rgpd/market/cookies.cookies_3_4_1') !!}</p>
         <ul>
-            <li><p>{!! __('rgpd-cookies.cookies_3_4_2') !!}</p></li>
-            <li><p>{!! __('rgpd-cookies.cookies_3_4_3') !!}</p></li>
-        </ul>
-
-        <h5>{!! __('rgpd-cookies.cookies_3_5') !!}</h5>
-        <p>{!! __('rgpd-cookies.cookies_3_5_1') !!}</p>
-        <ul>
-            <li><p>{!! __('rgpd-cookies.cookies_3_5_2') !!}</p></li>
+            <li><p>{!! __('rgpd/market/cookies.cookies_3_4_2') !!}</p></li>
+            <li><p>{!! __('rgpd/market/cookies.cookies_3_4_3') !!}</p></li>
         </ul>
 
-        <h5>{!! __('rgpd-cookies.cookies_3_6') !!}</h5>
-        <p>{!! __('rgpd-cookies.cookies_3_6_1') !!}</p>
+        <h5>{!! __('rgpd/market/cookies.cookies_3_5') !!}</h5>
+        <p>{!! __('rgpd/market/cookies.cookies_3_5_1') !!}</p>
         <ul>
-            <li><p>{!! __('rgpd-cookies.cookies_3_6_2') !!}</p></li>
-        </ul>
-        <p>{!! __('rgpd-cookies.cookies_3_6_3') !!}</p>
-        <p>{!! __('rgpd-cookies.cookies_3_6_4') !!}</p>
-        <ul>
-            <li><p>{!! __('rgpd-cookies.cookies_3_6_5') !!}</p></li>
-            <li><p>{!! __('rgpd-cookies.cookies_3_6_6') !!}</p></li>
-            <li><p>{!! __('rgpd-cookies.cookies_3_6_7', ['trademark' => config('pulsar-rgpd.trademark')]) !!}</p></li>
-            <li><p>{!! __('rgpd-cookies.cookies_3_6_8') !!}</p></li>
-            <li><p>{!! __('rgpd-cookies.cookies_3_6_9') !!}</p></li>
-        </ul>
-        <p>{!! __('rgpd-cookies.cookies_3_6_10', ['company' => config('pulsar-rgpd.company')]) !!}</p>
-
-        <h3>{!! __('rgpd-cookies.cookies_4') !!}</h3>
-        <p>{!! __('rgpd-cookies.cookies_4_1') !!}</p>
-
-        <p>{!! __('rgpd-cookies.cookies_4_2') !!}</p>
-        <ul>
-            <li><p>{!! __('rgpd-cookies.cookies_4_2_1') !!}</p></li>
-            <li><p>{!! __('rgpd-cookies.cookies_4_2_2') !!}</p></li>
-            <li><p>{!! __('rgpd-cookies.cookies_4_2_3') !!}</p></li>
-            <li><p>{!! __('rgpd-cookies.cookies_4_2_4') !!}</p></li>
+            <li><p>{!! __('rgpd/market/cookies.cookies_3_5_2') !!}</p></li>
         </ul>
 
-        <p>{!! __('rgpd-cookies.cookies_4_3') !!}</p>
-        <p>{!! __('rgpd-cookies.cookies_4_4', ['update_date' => config('pulsar-rgpd.update_date')]) !!}</p>
+        <h5>{!! __('rgpd/market/cookies.cookies_3_6') !!}</h5>
+        <p>{!! __('rgpd/market/cookies.cookies_3_6_1') !!}</p>
+        <ul>
+            <li><p>{!! __('rgpd/market/cookies.cookies_3_6_2') !!}</p></li>
+        </ul>
+        <p>{!! __('rgpd/market/cookies.cookies_3_6_3') !!}</p>
+        <p>{!! __('rgpd/market/cookies.cookies_3_6_4') !!}</p>
+        <ul>
+            <li><p>{!! __('rgpd/market/cookies.cookies_3_6_5') !!}</p></li>
+            <li><p>{!! __('rgpd/market/cookies.cookies_3_6_6') !!}</p></li>
+            <li><p>{!! __('rgpd/market/cookies.cookies_3_6_7', ['trademark' => config('pulsar-rgpd.trademark')]) !!}</p></li>
+            <li><p>{!! __('rgpd/market/cookies.cookies_3_6_8') !!}</p></li>
+            <li><p>{!! __('rgpd/market/cookies.cookies_3_6_9') !!}</p></li>
+        </ul>
+        <p>{!! __('rgpd/market/cookies.cookies_3_6_10', ['company' => config('pulsar-rgpd.company')]) !!}</p>
+
+        <h3>{!! __('rgpd/market/cookies.cookies_4') !!}</h3>
+        <p>{!! __('rgpd/market/cookies.cookies_4_1') !!}</p>
+
+        <p>{!! __('rgpd/market/cookies.cookies_4_2') !!}</p>
+        <ul>
+            <li><p>{!! __('rgpd/market/cookies.cookies_4_2_1') !!}</p></li>
+            <li><p>{!! __('rgpd/market/cookies.cookies_4_2_2') !!}</p></li>
+            <li><p>{!! __('rgpd/market/cookies.cookies_4_2_3') !!}</p></li>
+            <li><p>{!! __('rgpd/market/cookies.cookies_4_2_4') !!}</p></li>
+        </ul>
+
+        <p>{!! __('rgpd/market/cookies.cookies_4_3') !!}</p>
+        <p>{!! __('rgpd/market/cookies.cookies_4_4', ['update_date' => config('pulsar-rgpd.update_date')]) !!}</p>
     </div>
 </div>
